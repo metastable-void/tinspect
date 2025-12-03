ff7361f3abc3d89dce2fc394bd0f9a961347df1c
metastable-void <git@yuka.h09.eu>
2025-12-03 19:46:03 +0900

    bugfix

--- src/tproxy/mod.rs
+++ src/tproxy/mod.rs
@@ -1,7 +1,7 @@
 use rustls::{ClientConfig, RootCertStore, ServerConfig};
 use socket2::{Domain, Protocol, Socket, Type};
 use std::convert::Infallible;
-use std::net::{Ipv6Addr, SocketAddr, SocketAddrV4, SocketAddrV6};
+use std::net::{Ipv4Addr, Ipv6Addr, SocketAddr, SocketAddrV4, SocketAddrV6};
 use std::os::fd::AsRawFd;
 use std::path::Path;
 use std::sync::{Arc, OnceLock};
@@ -328,6 +328,18 @@ fn make_tproxy_listener(port: u16) -> std::io::Result<std::net::TcpListener> {
         if ret < 0 {
             return Err(std::io::Error::last_os_error());
         }
+
+        let ret = libc::setsockopt(
+            fd,
+            libc::SOL_IP,
+            libc::IP_TRANSPARENT,
+            &optval as *const _ as *const libc::c_void,
+            std::mem::size_of::<libc::c_int>() as libc::socklen_t,
+        );
+
+        if ret < 0 {
+            return Err(std::io::Error::last_os_error());
+        }
     }
 
     // bind to [::]:port
@@ -355,38 +367,79 @@ fn get_original_dst(stream: &TcpStream) -> std::io::Result<SocketAddr> {
         )
     };
 
+    if ret == 0 {
+        if len as usize != std::mem::size_of::<libc::sockaddr_in6>() {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "unexpected sockaddr length from IP6T_SO_ORIGINAL_DST",
+            ));
+        }
+
+        let addr = unsafe { addr.assume_init() };
+
+        if addr.sin6_family != libc::AF_INET6 as libc::sa_family_t {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "unexpected address family from IP6T_SO_ORIGINAL_DST",
+            ));
+        }
+
+        let port = u16::from_be(addr.sin6_port);
+        let flowinfo = u32::from_be(addr.sin6_flowinfo);
+        let scope_id = addr.sin6_scope_id;
+        let ipv6 = Ipv6Addr::from(addr.sin6_addr.s6_addr);
+
+        if let Some(ipv4) = ipv6.to_ipv4_mapped() {
+            return Ok(SocketAddr::V4(SocketAddrV4::new(ipv4, port)));
+        } else {
+            return Ok(SocketAddr::V6(SocketAddrV6::new(
+                ipv6, port, flowinfo, scope_id,
+            )));
+        }
+    }
+
+    let err = std::io::Error::last_os_error();
+    match err.raw_os_error() {
+        Some(code) if code == libc::ENOPROTOOPT || code == libc::EINVAL => {}
+        _ => return Err(err),
+    }
+
+    let mut addr = std::mem::MaybeUninit::<libc::sockaddr_in>::zeroed();
+    let mut len = std::mem::size_of::<libc::sockaddr_in>() as libc::socklen_t;
+
+    let ret = unsafe {
+        libc::getsockopt(
+            fd,
+            libc::SOL_IP,
+            libc::SO_ORIGINAL_DST,
+            addr.as_mut_ptr() as *mut libc::c_void,
+            &mut len,
+        )
+    };
+
     if ret < 0 {
         return Err(std::io::Error::last_os_error());
     }
 
-    if len as usize != std::mem::size_of::<libc::sockaddr_in6>() {
+    if len as usize != std::mem::size_of::<libc::sockaddr_in>() {
         return Err(std::io::Error::new(
             std::io::ErrorKind::Other,
-            "unexpected sockaddr length from IP6T_SO_ORIGINAL_DST",
+            "unexpected sockaddr length from SO_ORIGINAL_DST",
         ));
     }
 
     let addr = unsafe { addr.assume_init() };
 
-    if addr.sin6_family != libc::AF_INET6 as libc::sa_family_t {
+    if addr.sin_family != libc::AF_INET as libc::sa_family_t {
         return Err(std::io::Error::new(
             std::io::ErrorKind::Other,
-            "unexpected address family from IP6T_SO_ORIGINAL_DST",
+            "unexpected address family from SO_ORIGINAL_DST",
         ));
     }
 
-    let port = u16::from_be(addr.sin6_port);
-    let flowinfo = u32::from_be(addr.sin6_flowinfo);
-    let scope_id = addr.sin6_scope_id;
-    let ipv6 = Ipv6Addr::from(addr.sin6_addr.s6_addr);
-
-    if let Some(ipv4) = ipv6.to_ipv4_mapped() {
-        Ok(SocketAddr::V4(SocketAddrV4::new(ipv4, port)))
-    } else {
-        Ok(SocketAddr::V6(SocketAddrV6::new(
-            ipv6, port, flowinfo, scope_id,
-        )))
-    }
+    let port = u16::from_be(addr.sin_port);
+    let ipv4 = Ipv4Addr::from(u32::from_be(addr.sin_addr.s_addr));
+    Ok(SocketAddr::V4(SocketAddrV4::new(ipv4, port)))
 }
 
 fn bind(port: u16) -> std::io::Result<TcpListener> {
@@ -870,11 +923,23 @@ pub async fn handler<S: AsyncRead + AsyncWrite + Unpin + Send + 'static>(
         return Ok(resp);
     }
 
+    let empty_req = Arc::new(empty_req);
     let is_tls = !is_plain_tcp::<S>();
     let is_tls_2 = is_tls_stream::<S>();
     debug_assert_eq!(is_tls, is_tls_2, "Unsupported transport");
+    let http_ctx = HttpContext {
+        is_tls,
+        sockinfo,
+        req: empty_req.clone(),
+    };
+
+    let req = match state.process_http_request(req, http_ctx.clone()) {
+        Err(res) => return Ok(res),
+        Ok(req) => req,
+    };
+
     let mut client = if is_tls {
-        let server_name = match server_name_from_req(&empty_req, &sockinfo) {
+        let server_name = match server_name_from_req(empty_req.as_ref(), &sockinfo) {
             Err(_e) => {
                 let res = Response::builder()
                     .status(400)
@@ -935,6 +1000,8 @@ pub async fn handler<S: AsyncRead + AsyncWrite + Unpin + Send + 'static>(
 
         Ok(res) => res,
     };
+
+    let res = state.process_http_response(res, http_ctx);
 
     Ok(res)
 }
