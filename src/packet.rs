2955c57b78439297c23078d24409548f17606322
metastable-void <git@yuka.h09.eu>
2025-12-03 21:55:03 +0900

    more doc

--- src/packet.rs
+++ src/packet.rs
@@ -1,5 +1,6 @@
 use std::net::SocketAddr;
 
+/// Raw information about the proxied connection.
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Copy)]
 pub struct SocketInfo {
     pub client_addr: SocketAddr,
