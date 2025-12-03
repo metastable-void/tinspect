615ea92a4c76b1bfb7a0b9c2109a7a982788940a
metastable-void <git@yuka.h09.eu>
2025-12-03 21:19:10 +0900

    reduce exposure

--- src/tproxy/mod.rs
+++ src/tproxy/mod.rs
@@ -7,7 +7,5 @@ mod transport;
 mod ws;
 
 pub use context::{HttpContext, ProxyState, WebSocketContext};
-pub use http::handler;
-pub use server::{run_port80, run_port443, serve_one_connection};
+pub use server::{run_port80, run_port443};
 pub use tls::TlsMitmState;
-pub use ws::{create_upstream_ws, handle_ws, is_ws_upgrade, ws_handshake_response};
